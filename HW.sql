0b8278c6fc53a0b0aa2ae08dbfa1e07b4a52a8b4
Константин Пигулевский <pigulevskii2000@gmail.com>
2023-07-01 21:14:32 +0300

    HW corrected

--- HW.sql
+++ HW.sql
@@ -4,13 +4,6 @@ name VARCHAR(40) unique,
 release_date date
 );
 
-create table if not exists tracks (
-tracks_id SERIAL primary key,
-compilation_id INTEGER not null references compilation(compilation_id),
-name VARCHAR(100),
-duration DECIMAL(3, 2) check(duration<12)
-);
-
 create table if not exists album (
 album_id SERIAL primary key,
 name VARCHAR(60) unique,
@@ -18,9 +11,18 @@ release_date date
 );
 
 create table if not exists track (
-track_id INTEGER primary key references album(album_id),
+track_id SERIAL primary key,
 name VARCHAR(100),
-duration DECIMAL(3, 2) check(duration<12)
+duration DECIMAL(3, 2) check(duration<12),
+album_belonging INTEGER references album(album_id)
+);
+
+create table if not exists compilation_track (
+compilation_track_id SERIAL primary key,
+compilation_id INT not null,
+track_id INT not null,
+constraint fk_compilation foreign key(compilation_id) references compilation(compilation_id) on delete cascade,
+constraint fk_track foreign key(track_id) references track(track_id) on delete cascade
 );
 
 create table if not exists artist (
@@ -48,3 +50,6 @@ genre_id INT not null,
 constraint fk_artist foreign key(artist_id) references artist(artist_id) on delete cascade,
 constraint fk_genre foreign key(genre_id) references genre(genre_id) on delete cascade
 );
+
+
+
